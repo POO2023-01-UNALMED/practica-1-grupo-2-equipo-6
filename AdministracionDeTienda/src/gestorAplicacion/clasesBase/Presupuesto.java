66cd2fb0ccb136846a22d4ccfc98889b0cc1de10
meg63 <mechavarriac@unal.edu.co>
2023-04-05 13:57:23 -0500

    Update Presupuesto.java

--- AdministracionDeTienda/src/gestorAplicacion/clasesBase/Presupuesto.java
+++ AdministracionDeTienda/src/gestorAplicacion/clasesBase/Presupuesto.java
@@ -2,7 +2,7 @@ package gestorAplicacion.clasesBase;
 
 public class Presupuesto {
 	private static double gananciaBruta;
-	public double pagoNomina() {
+	static public double disponibleNomina() {
 		return gananciaBruta*0.36;
 	}
 	public double getGananciaBruta() {
@@ -13,6 +13,17 @@ public class Presupuesto {
 	public void setGananciaBruta(double ganancia) {
 		gananciaBruta=ganancia;
 		
+	}
+	
+	public String verificacionPago(double pago) {
+		if(pago<=disponibleNomina()) {
+			return "Se ha hecho el pago exitosamente";
+		}else if(pago>disponibleNomina()) {
+			return "El dinero disponible para el pago de la nomina no es suficiente para realizar el pago";
+		}else {
+			return "No se ha realizado el pago";
+		}
+	
 	}
 	
 
