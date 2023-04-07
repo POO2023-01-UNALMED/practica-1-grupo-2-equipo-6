cfdbacc4bb306ad1e021086760d36e322f87ff38
gabdevcol <gabrielantonioserrano28@gmail.com>
2023-04-07 16:04:51 -0700

    Actualizacion de Proveedor

--- AdministracionDeTienda/src/gestorAplicacion/clasesHerencia/Proveedor.java
+++ AdministracionDeTienda/src/gestorAplicacion/clasesHerencia/Proveedor.java
@@ -1,5 +1,39 @@
 package gestorAplicacion.clasesHerencia;
+import java.util.ArrayList;
 
-public class Proveedor {
+import gestorAplicacion.clasesBase.Persona;
+import gestorAplicacion.clasesBase.Producto;
+
+public class Proveedor extends Persona {
+	private int numeroVentas;  
+	
+	public Proveedor(String nombre, int cedula, int numero) {
+		super(nombre,cedula);
+		this.numeroVentas = numero;
+	}
+	
+	public ArrayList<ArrayList<Object>> verificarDisponibilidad(ArrayList<ArrayList<Object>> lista) {
+		
+		for(int i=0; i< lista.size();i++) {
+			int aleatorio = (int)(Math.random()*5+1);
+			if (aleatorio == 3) {
+				lista.get(i).set(2, false);
+			}
+			else {
+				lista.get(i).set(2, true);
+			}
+		}
+		
+		return lista;
+	}
+	
+	public void setCalificacion() { numeroVentas++;}
+	
+	public int getCalificacion() {return numeroVentas;}
+	
+	public String toString() {
+		return "Numero de Ventas: "+ numeroVentas;
+		
+	}
 
 }
