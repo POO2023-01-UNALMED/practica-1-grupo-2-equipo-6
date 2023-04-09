f5bd66ffacd59424609104fb78d953258d7082ee
Carmen Calderón <cacalderonc@unal.edu.co>
2023-04-09 13:19:43 -0500

    CreacionOfertas

--- AdministracionDeTienda/src/gestorAplicacion/clasesBase/Nomina.java
+++ AdministracionDeTienda/src/gestorAplicacion/clasesBase/Nomina.java
@@ -8,19 +8,19 @@ public class Nomina {
 	meses mes;
 	public static ArrayList<meses> disponibles=new ArrayList<meses>();
 	public static ArrayList<Empleado> empleados=new ArrayList<Empleado>();
-	
-	
-	
-	public Nomina(meses mes) {
+
+
+
+	public Nomina(meses mes, ArrayList<Empleado>empleados) {
 		this.mes=mes;
-		disponibles.remove(mes);
-		
+		//disponibles.remove(mes);
+		Nomina.empleados=empleados;
+
 	}
-	
+
 	public enum meses{
 			ENERO(1),FEBRERO(2),MARZO(3),ABRIL(4),MAYO(5),JUNIO(6),JULIO(7),AGOSTO(8),SEPTIEMBRE(9),OCTUBRE(10),NOVIEMBRE(11),DICIEMBRE(12);
 
-		
 			private int num;
 			meses(int num) {
 				this.setNum(num);
@@ -31,11 +31,66 @@ public class Nomina {
 			public void setNum(int num) {
 				this.num = num;
 			}
-		
+
 	}
 	public ArrayList<Empleado> empleados() {
 		//recorrer la lista de ventas y sacar los objetos que tengan el mes que se pide y añadir a esa lista, los empleados y las comisiones
 		return empleados;
 	}
+
+
+	public static ArrayList<meses> getDisponibles() {
+		return disponibles;
+	}
+
+
+
+
+	public static void setDisponibles(ArrayList<meses> disponibles) {
+		Nomina.disponibles = disponibles;
+	}
+
+
+
+
+	public static ArrayList<Empleado> getEmpleados() {
+		return empleados;
+	}
+
+
+
+
+	public static void setEmpleados(ArrayList<Empleado> empleados) {
+		Nomina.empleados = empleados;
+	}
+
+	public static ArrayList<Venta> informarVentas(Empleado empleado) {
+		ArrayList<Venta> Ventas=new ArrayList<Venta>();
+
+		if(empleado.getVentas().size()!=0) {
+				for (Venta venta: empleado.getVentas()) {
+					Ventas.add(venta);
+				}
+		return Ventas;
+		}
+		else{
+			return null;
+		}
+	}
+
+	public static ArrayList<Venta> informarVentas() {
+
+		ArrayList<Venta> Ventas=new ArrayList<Venta>();
+		for (Empleado empleado: empleados) {
+			if (informarVentas(empleado)!=null) {
+				Ventas.addAll(informarVentas(empleado));
+			}
+		}
+
+
+		return Ventas;
+
+	}
+
 
 }
--- AdministracionDeTienda/src/gestorAplicacion/clasesBase/Tienda.java
+++ AdministracionDeTienda/src/gestorAplicacion/clasesBase/Tienda.java
@@ -5,19 +5,35 @@ import java.util.ArrayList;
 public class Tienda {
 	private double presupuestoCompras;
 	private ArrayList<Object> proveedores = new ArrayList<Object>();
-	
+	private Nomina nomina;
+
 	public Tienda(int presupuesto, ArrayList<Object> proveedores){
 		presupuestoCompras = presupuesto;
 		this.proveedores = proveedores;
 	}
-	
-	public void setPresupuestoCompra(double dinero) { presupuestoCompras = dinero;}
-	
-	public double getPresupuestoCompra() {return presupuestoCompras;}
-	
-	public void setProveedores(ArrayList<Object> proveedores) {this.proveedores = proveedores;}
-	
-	public ArrayList<Object> getProveedores(){return proveedores;}
-}
-	
 
+	public void setPresupuestoCompra(double dinero) { presupuestoCompras = dinero;}
+
+	public double getPresupuestoCompra() {return presupuestoCompras;}
+
+	public void setProveedores(ArrayList<Object> proveedores) {this.proveedores = proveedores;}
+
+	public ArrayList<Object> getProveedores(){return proveedores;}
+
+	public Nomina getNomina(){return Nomina;}
+
+	public setNomina(Nomina nomina){this.nomina=nomina;}
+
+	public boolean generarOferta() {
+		double totalVentas=0.0;
+		for(Venta v:Nomina.informarVentas()) {
+
+			totalVentas+=v.getTotal();
+		}
+		if(presupuestoCompras<=totalVentas) {
+			return true;
+		}
+		return false;
+	}
+
+}
