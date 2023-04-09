017aa001ab3776b40146a8d17f187156625ad2de
Carmen Calderón <cacalderonc@unal.edu.co>
2023-04-09 13:54:08 -0500

    ModificacionTienda

--- AdministracionDeTienda/src/gestorAplicacion/clasesBase/Tienda.java
+++ AdministracionDeTienda/src/gestorAplicacion/clasesBase/Tienda.java
@@ -6,31 +6,34 @@ public class Tienda {
 	private double presupuestoCompras;
 	private ArrayList<Object> proveedores = new ArrayList<Object>();
 	private Nomina nomina;
+	private Bodega bodega;
 
-	public Tienda(int presupuesto, ArrayList<Object> proveedores){
-		presupuestoCompras = presupuesto;
+	public Tienda(int presupuesto, ArrayList<Object> proveedores, Nomina nomina, Bodega bodega){
+		this.presupuestoCompras = presupuesto;
 		this.proveedores = proveedores;
+		this.bodega=bodega;
+		this.nomina=nomina;
 	}
 
-	public void setPresupuestoCompra(double dinero) { presupuestoCompras = dinero;}
+  public void setBodega(Bodega bodega){this.bodega=bodega;}
+	public Bodega getPresupuestoCompras() {return this.bodega;}
 
-	public double getPresupuestoCompra() {return presupuestoCompras;}
+
+	public void setPresupuestoCompras(double dinero) { presupuestoCompras = dinero;}
+
+ public double getPresupuestoCompras() {return presupuestoCompras;}
 
 	public void setProveedores(ArrayList<Object> proveedores) {this.proveedores = proveedores;}
 
 	public ArrayList<Object> getProveedores(){return proveedores;}
 
-	public Nomina getNomina(){return Nomina;}
+	public Nomina getNomina(){return nomina;}
 
 	public setNomina(Nomina nomina){this.nomina=nomina;}
 
 	public boolean generarOferta() {
-		double totalVentas=0.0;
-		for(Venta v:Nomina.informarVentas()) {
-
-			totalVentas+=v.getTotal();
-		}
-		if(presupuestoCompras<=totalVentas) {
+		double totalVentas=Nomina.informarVentas().size();
+		if(totalVentas/bodega.getProductosEnBodega().size()>0.5) {
 			return true;
 		}
 		return false;
