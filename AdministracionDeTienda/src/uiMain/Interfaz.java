8d1738a769d73e0114710ab733d14ed5156f521e
Carmen Calderón <cacalderonc@unal.edu.co>
2023-04-10 12:16:36 -0500

    Update Interfaz.java
    Parte de la funcionalidad para creación de ofertas

--- AdministracionDeTienda/src/uiMain/Interfaz.java
+++ AdministracionDeTienda/src/uiMain/Interfaz.java
@@ -15,11 +15,63 @@ public class Interfaz {
 		sc.nextLine();
 		return sc.nextLine();
 	}
+
+	public void crearOferta() {
+			Tienda tiendaOferta;
+			ArrayList<Tienda> tiendasOferta=new ArrayList<Tienda>();
+
+			for(Tienda t: tiendas) {
+				if(t.generarOferta()) {
+					tiendasOferta.add(t);
+				}
+			}
+			if (tiendasOferta.size()!=0) {
+				String s="";
+				Byte i=0;
+				for(Tienda t: tiendasOferta) {
+					i++;
+					s+=String.valueOf(i)+t.getNombre()+"\n";
+				}
+				System.out.println("Seleccione la tienda en la cual desea crear una oferta: ");
+
+				Byte opcion=sc.nextByte();
+				do{
+					System.out.println("Ingrese una opcion valida: ");
+					opcion=sc.nextByte();
+
+				}while(opcion<=0 || opcion>tiendasOferta.size());
+
+				tiendaOferta=tiendasOferta.get(opcion-1);
+
+				ArrayList<String> tiposDisponibles=tiendaOferta.tiposProductos();
+
+				if(tiposDisponibles.size()!=0) {
+					System.out.println("No hay productos disponibles en esta tienda");
+				}
+				else {
+					Byte contador=0;
+					for(String m:tiposDisponibles) {
+						contador++;
+						System.out.println(contador+". "+m);
+					}
+					System.out.println("Ingrese el tipo de producto al cual desea aplicar la oferta: ");
+					Byte opcion2=sc.nextByte();
+					do{
+						System.out.println("Ingrese una opcion valida: ");
+						opcion2=sc.nextByte();
+
+					}while(opcion2<=0 || opcion2>contador);
+
+					ArrayList<Object>mercanciaDisponibleDeTipo=tiendaOferta.getBodega().disponibilidadProductos(tiposDisponibles.get(opcion2-1));		
+				}
+			}
+		}
+
 	public static void adminNomina() {
 		System.out.println("Seleccione uno de los meses disponibles\n");
 		for(int mes=0;mes<Nomina.disponibles.size();mes++) {
 			System.out.println(Nomina.disponibles.get(mes).getNum()+". "+Nomina.disponibles.get(mes));
-			
+
 		}
 		long opcion=readLong();
 		meses mesE=null;
@@ -36,7 +88,7 @@ public class Interfaz {
 			if(Venta.ventas.get(i).getMes()==mesE) {
 				seleccionadas.add(Venta.ventas.get(i));
 				integrantes.add(Venta.ventas.get(i).getEmpleado());
-				
+
 			}
 		}
 		for (int i=0;i<integrantes.size();i++) {
@@ -48,20 +100,20 @@ public class Interfaz {
 				}
 			}
 			//Aqui debe ir la relacion del sueldo con la comisiones
-			integrantes.get(i).setSueldo(sueldo);		
+			integrantes.get(i).setSueldo(sueldo);
 		}
 		System.out.println("\nLos elementos de esta nomina son:");
 		System.out.println("\nEmpleado Sueldo");
 		for(int i=0;i<integrantes.size();i++) {
 			Empleado a = integrantes.get(i);
 			System.out.println(a.getNombre()+" "+a.getSueldo());
-			
+
 		}
-		
-		
-		
-		
+
+
+
+
 	}
-	
+
 
 }
