efd3a5fcecc1ceeda417f9716c140a5176efc245
gabdevcol <gabrielantonioserrano28@gmail.com>
2023-04-09 19:22:35 -0700

    Actualizacion de la clase bodega y la clase compra

--- AdministracionDeTienda/src/gestorAplicacion/clasesBase/Bodega.java
+++ AdministracionDeTienda/src/gestorAplicacion/clasesBase/Bodega.java
@@ -1,47 +1,43 @@
 package gestorAplicacion.clasesBase;
 
 import java.util.ArrayList;
+import java.io.*;
 
-public class Bodega {
-	private final int capacidadMaxima;
-	private int numeroDeProductosEnBodega;
-	private final int[] stopCamiseta,stopPantalon,stopPantaloneta,stopAbrigo,stopBuzo;
+public class Bodega implements java.io.Serializable{
+
+	private final int stopCamiseta, stopPantalon, stopAbrigo;
 	private ArrayList<ArrayList<Object>> productosEnBodega = new ArrayList<ArrayList<Object>>();
-
-	public Bodega(int capacidadMax, ArrayList<Object> productosEnBodega, int[] camiseta,int[] pantalon, int[] pantaloneta, int[] abrigo, int[] buzo) {
-		capacidadMaxima = capacidadMax;
+	
+	
+	public Bodega( int camiseta,int pantalon, int abrigo) {
 		stopCamiseta = camiseta;
 		stopPantalon = pantalon;
-		stopPantaloneta = pantaloneta;
-		stopAbrigo = abrigo;
-		stopBuzo = buzo;
-		this.productosEnBodega.add(productosEnBodega);
+		stopAbrigo = abrigo;	
 	}
-
+		
 	public void agregarProductos(ArrayList<Object> producto) {
 		this.productosEnBodega.add(producto);
 	}
-
-	public int calcularNumeroProductos(ArrayList<ArrayList<Object>> productosEnBodega) {
+	
+	public int calcularNumeroProductos() {
 		int numeroDeProductos = 0;
 		for (int i = 0; i < productosEnBodega.size(); i++) {
 			numeroDeProductos += productosEnBodega.get(i).get(1).hashCode();
 		}
 		return numeroDeProductos;
 	}
-
-	public int calcularNumeroCamisetas(ArrayList<ArrayList<Object>> productosEnBodega) {
+	
+	public int calcularNumeroCamisetas() {
 		int numeroCamisetas = 0;
 		for (int i = 0; i < productosEnBodega.size(); i++) {
-//			System.out.println(productosEnBodega.get(i)[1].hashCode());
 			if(((Producto) productosEnBodega.get(i).get(0)).getCodigo().substring(0, 1).equals("0")) {
 				numeroCamisetas += productosEnBodega.get(i).get(1).hashCode();
 			}
 		}
-			return numeroCamisetas;
+		return numeroCamisetas;
 	}
-
-	public int calcularNumeroPantalon(ArrayList<ArrayList<Object>> productosEnBodega) {
+	
+	public int calcularNumeroPantalon() {
 		int numeroPantalones = 0;
 		for (int i = 0; i < productosEnBodega.size(); i++) {
 			if(((Producto) productosEnBodega.get(i).get(0)).getCodigo().substring(0, 1).equals("1")) {
@@ -50,8 +46,8 @@ public class Bodega {
 		}
 		return numeroPantalones;
 	}
-
-	public int calcularNumeroAbrigo(ArrayList<ArrayList<Object>> productosEnBodega) {
+	
+	public int calcularNumeroAbrigo() {
 		int numeroAbrigo = 0;
 		for (int i = 0; i < productosEnBodega.size(); i++) {
 			if(((Producto) productosEnBodega.get(i).get(0)).getCodigo().substring(0, 1).equals("2")) {
@@ -60,49 +56,27 @@ public class Bodega {
 		}
 		return numeroAbrigo;
 	}
-
-	public int calcularNumeroBuzo(ArrayList<ArrayList<Object>> productosEnBodega) {
-		int numeroBuzo = 0;
-		for (int i = 0; i < productosEnBodega.size(); i++) {
-			if(((Producto) productosEnBodega.get(i).get(0)).getCodigo().substring(0, 1).equals("3")) {
-				numeroBuzo += productosEnBodega.get(i).get(1).hashCode();
-			}
-		}
-		return numeroBuzo;
-	}
-
-	public int calcularNumeroPantalonetas(ArrayList<ArrayList<Object>> productosEnBodega) {
-		int numeroPantaloneta = 0;
-		for (int i = 0; i < productosEnBodega.size(); i++) {
-			if(((Producto) productosEnBodega.get(i).get(0)).getCodigo().substring(0, 1).equals("4")) {
-				numeroPantaloneta += productosEnBodega.get(i).get(1).hashCode();
-			}
-		}
-		return numeroPantaloneta;
-	}
-
-	public void setNumeroDeProductosEnBodega(int valor) {numeroDeProductosEnBodega = valor;}
+			
+	
 	public void setProductosEnBodega(ArrayList<ArrayList<Object>> valor) {productosEnBodega = valor;}
-
-	public int getCapacidadMaxima() {return capacidadMaxima;}
-	public int getNumeroDeProductosEnBodega() {return numeroDeProductosEnBodega;}
-	public int[] getStopCamiseta() {return stopCamiseta;}
-	public int[] getStopPantalon() {return stopPantalon;}
-	public int[] getStopPantaloneta() {return stopPantaloneta;}
-	public int[] getStopAbrigo() {return stopAbrigo;}
-	public int[] getStopBuzo() {return stopBuzo;}
+	
+	public int getStopCamiseta() {return stopCamiseta;}
+	
+	public int getStopPantalon() {return stopPantalon;}
+	
+	public int getStopAbrigo() {return stopAbrigo;}
+	
 	public ArrayList<ArrayList<Object>> getProductosEnBodega() {return productosEnBodega;}
-
-
+	
 	public String toString() {
 		int numeroDeProductos = 0;
 		for (int i = 0; i < productosEnBodega.size(); i++) {
 			numeroDeProductos += productosEnBodega.get(i).get(1).hashCode();
 		}
-
-		return "capacidad maxima = " + capacidadMaxima + "\nproductos en bodega = " + numeroDeProductos;
+		
+		return "productos en bodega = " + numeroDeProductos;
 	}
-	//Regresa los productos de cierto tipo- Omite productos repetidos
+	
 	public ArrayList<Object> disponibilidadProductos(String tipo) {
 		ArrayList<Object> productosTipo=new ArrayList<Object>();
 		for(ArrayList<Object> i: productosEnBodega) {
@@ -114,7 +88,6 @@ public class Bodega {
 				Object[] lista1={(Producto)i.get(0), cantidad};
 				productosTipo.add(lista1);
 			}
-
 		}
 		return productosTipo;
 	}
--- AdministracionDeTienda/src/gestorAplicacion/clasesBase/Compra.java
+++ AdministracionDeTienda/src/gestorAplicacion/clasesBase/Compra.java
@@ -9,18 +9,21 @@ public class Compra {
 	static int codigo;
 
 	private ArrayList<ArrayList<Object>> listaCompra = new ArrayList<ArrayList<Object>>();
+	private ArrayList<Object> proveedores = new ArrayList<Object>();
 	private Bodega bodega;
-	private Tienda tienda;
 	private double costo = 0;
+	private Tienda tienda;
 		
 	public Compra(Bodega bodega, Tienda tienda) {
 		this.bodega = bodega;
+		this.proveedores = tienda.getProveedores();
 		this.tienda = tienda;
 		
 		buscarProductosNecesitados();
 		selecionarCantidadDeProductos();
-		consultarProveedores(tienda);
+		consultarProveedores(this.proveedores);
 		calcularCosto();
+		actualizarBodega();
 		codigo++;
 	}
 	
@@ -28,26 +31,21 @@ public class Compra {
 	
 		for(int i = 0; i < bodega.getProductosEnBodega().size(); i++) {
 			
-			if(((Producto) bodega.getProductosEnBodega().get(i).get(0)).getCodigo().substring(0,1).equals("0") && bodega.getProductosEnBodega().get(i).get(1).hashCode()< bodega.getStopCamiseta()[1] - bodega.getStopCamiseta()[0]) {
-					bodega.getProductosEnBodega().get(i).set(2, bodega.getStopCamiseta()[0] - bodega.getProductosEnBodega().get(i).get(1).hashCode());
-					listaCompra.add(bodega.getProductosEnBodega().get(i));
+			if(((Producto) bodega.getProductosEnBodega().get(i).get(0)).getCodigo().substring(0,1).equals("0") && bodega.getStopCamiseta() - bodega.getProductosEnBodega().get(i).get(1).hashCode() >=6) {
+					bodega.getProductosEnBodega().get(i).set(2, bodega.getStopCamiseta() - bodega.getProductosEnBodega().get(i).get(1).hashCode());
+					ArrayList<Object> clon =(ArrayList<Object>)bodega.getProductosEnBodega().get(i).clone();
+					listaCompra.add(clon);
 			}
-			else if(((Producto) bodega.getProductosEnBodega().get(i).get(0)).getCodigo().substring(0,1).equals("1") && bodega.getProductosEnBodega().get(i).get(1).hashCode()< bodega.getStopPantalon()[1] - bodega.getStopPantalon()[0]){
-					bodega.getProductosEnBodega().get(i).set(2,bodega.getStopPantalon()[0] - bodega.getProductosEnBodega().get(i).get(1).hashCode());
-					listaCompra.add(bodega.getProductosEnBodega().get(i));
+			else if(((Producto) bodega.getProductosEnBodega().get(i).get(0)).getCodigo().substring(0,1).equals("1") && bodega.getStopPantalon()-bodega.getProductosEnBodega().get(i).get(1).hashCode() >=6){
+					bodega.getProductosEnBodega().get(i).set(2,bodega.getStopPantalon() - bodega.getProductosEnBodega().get(i).get(1).hashCode());
+					ArrayList<Object> clon =(ArrayList<Object>)bodega.getProductosEnBodega().get(i).clone();
+					listaCompra.add(clon);
 				
 			}
-			else if(((Producto) bodega.getProductosEnBodega().get(i).get(0)).getCodigo().substring(0,1).equals("2") && bodega.getProductosEnBodega().get(i).get(1).hashCode()< bodega.getStopAbrigo()[1]-bodega.getStopAbrigo()[0]) {
-					bodega.getProductosEnBodega().get(i).set(2, bodega.getStopAbrigo()[0] - bodega.getProductosEnBodega().get(i).get(1).hashCode());
-					listaCompra.add(bodega.getProductosEnBodega().get(i));
-			}
-			else if(((Producto) bodega.getProductosEnBodega().get(i).get(0)).getCodigo().substring(0,1).equals("3") && bodega.getProductosEnBodega().get(i).get(1).hashCode()< bodega.getStopBuzo()[1] - bodega.getStopBuzo()[0]) {
-				bodega.getProductosEnBodega().get(i).set(2, bodega.getStopBuzo()[0] - bodega.getProductosEnBodega().get(i).get(1).hashCode());
-				listaCompra.add(bodega.getProductosEnBodega().get(i));
-			}
-			else if(((Producto) bodega.getProductosEnBodega().get(i).get(0)).getCodigo().substring(0,1).equals("4") && bodega.getProductosEnBodega().get(i).get(1).hashCode()< bodega.getStopPantaloneta()[1] - bodega.getStopPantaloneta()[0]) {
-				bodega.getProductosEnBodega().get(i).set(2, bodega.getStopPantaloneta()[0] - bodega.getProductosEnBodega().get(i).get(1).hashCode());
-				listaCompra.add(bodega.getProductosEnBodega().get(i));
+			else if(((Producto) bodega.getProductosEnBodega().get(i).get(0)).getCodigo().substring(0,1).equals("2") && bodega.getStopAbrigo() -bodega.getProductosEnBodega().get(i).get(1).hashCode()>= 6) {
+					bodega.getProductosEnBodega().get(i).set(2, bodega.getStopAbrigo() - bodega.getProductosEnBodega().get(i).get(1).hashCode());
+					ArrayList<Object> clon =(ArrayList<Object>)bodega.getProductosEnBodega().get(i).clone();
+					listaCompra.add(clon);
 			}
 		}
 	}
@@ -61,31 +59,27 @@ public class Compra {
 			numeros.add(listaOrdenada.get(i).get(1));
 		}
 		
-		int numeroCamisetas = bodega.calcularNumeroCamisetas(bodega.getProductosEnBodega());
-		int numeroPantalones = bodega.calcularNumeroPantalon(bodega.getProductosEnBodega());
-		int numeroAbrigos = bodega.calcularNumeroAbrigo(bodega.getProductosEnBodega());
-		int numeroBuzos = bodega.calcularNumeroBuzo(bodega.getProductosEnBodega());
-		int numeroPantalonetas = bodega.calcularNumeroPantalonetas(bodega.getProductosEnBodega());
+		int numeroCamisetas = bodega.calcularNumeroCamisetas();
+		int numeroPantalones = bodega.calcularNumeroPantalon();
+		int numeroAbrigos = bodega.calcularNumeroAbrigo();
 		int acumulado=0;
 		int control=0;
-//		int controlInterno = 1;
 		int  i=0;
 		
 		while (true) {
 			if (i==listaOrdenada.size()) {i=0;}
 			costoCompra += ((Producto) listaOrdenada.get(i).get(0)).getCosto()*6;
 			
-			if(tienda.getPresupuestoCompra() - costoCompra >= 0 ) {
+			if(tienda. getPresupuestoCompra() - costoCompra >= 0 ) {
 			
 				while( i<=listaOrdenada.size()-2) {
 					
 					acumulado = listaOrdenada.get(i).get(1).hashCode();
 					
-					if(((Producto) listaOrdenada.get(i).get(0)).getTipo().equals("camiseta") && numeroCamisetas + 6 <bodega.getStopCamiseta()[1] && tienda.getPresupuestoCompra() - costoCompra >= 0 ) {
+					if(((Producto) listaOrdenada.get(i).get(0)).getTipo().equals("camiseta") && numeroCamisetas + 6 <bodega.getStopCamiseta() && tienda. getPresupuestoCompra() - costoCompra >= 0 ) {
 						if(listaOrdenada.get(i).get(1).hashCode()<= listaOrdenada.get(i+1).get(1).hashCode()) {
 							if(listaOrdenada.get(i).get(1).hashCode()<=listaOrdenada.get(0).get(1).hashCode()) {
-//								System.out.println(tienda.getPresupuestoCompra());
-//								System.out.println(costoCompra);
+
 								acumulado += 6;
 								listaOrdenada.get(i).set(1, acumulado);
 								numeroCamisetas+=6;
@@ -107,13 +101,11 @@ public class Compra {
 						control++;
 						break;
 					}
-//					controlInterno++;
 				}
-//				controlInterno =1;
 				if(i==listaOrdenada.size()-1) {
 					acumulado = listaOrdenada.get(i).get(1).hashCode();
 					
-					if(((Producto) listaOrdenada.get(i).get(0)).getTipo().equals("camiseta") && numeroCamisetas + 6 <bodega.getStopCamiseta()[1]) {
+					if(((Producto) listaOrdenada.get(i).get(0)).getTipo().equals("camiseta") && numeroCamisetas + 6 <bodega.getStopCamiseta()) {
 						if(listaOrdenada.get(i).get(1).hashCode()<listaOrdenada.get(0).get(1).hashCode()) {
 							acumulado+=6;
 							listaOrdenada.get(i).set(1, acumulado);
@@ -133,12 +125,9 @@ public class Compra {
 				}
 			if(control == listaOrdenada.size()) {break;}
 			i++;
-			
 		}
 		
 		listaCompra = calcularPedido(numeros, listaOrdenada);
-		
-//		listaCompra = listaOrdenada;
 	}
 		
 	public ArrayList<ArrayList<Object>> ordenarListaCompra( ArrayList<ArrayList<Object>> lista){
@@ -164,8 +153,6 @@ public class Compra {
 		return listaOrdenada;	
 	}
 
-	
-	
 	public ArrayList<ArrayList<Object>> calcularPedido( ArrayList<Object> numeros,ArrayList<ArrayList<Object>> listaM){
 		int cantidad;
 		ArrayList<ArrayList<Object>> listaNueva = new ArrayList<ArrayList<Object>>();
@@ -178,18 +165,18 @@ public class Compra {
 		return listaNueva;
 	}
 	
-	public void consultarProveedores(Tienda tienda) {
-	Proveedor mejorProveedor =((Proveedor) tienda.getProveedores().get(0));
+	public void consultarProveedores(  ArrayList<Object> proveedores) {
+	Proveedor mejorProveedor =((Proveedor) proveedores.get(0));
 	int cantidadDisponible = 0;
 	
 	ArrayList<ArrayList<Object>> listaFinal = new ArrayList<>();
 	ArrayList<ArrayList<Object>> l = new ArrayList<>();
 	
-		for (int i =0; i < tienda.getProveedores().size(); i++) {
-			l = ((Proveedor) tienda.getProveedores().get(i)).verificarDisponibilidad(listaCompra);
+		for (int i =0; i < proveedores.size(); i++) {
+			l = ((Proveedor) proveedores.get(i)).verificarDisponibilidad(listaCompra);
 			if(cantidadDisponible<= Collections.frequency(l.get(0), true)) {
 				cantidadDisponible = Collections.frequency(l.get(0), true);
-				mejorProveedor = ((Proveedor) tienda.getProveedores().get(i));
+				mejorProveedor = ((Proveedor) proveedores.get(i));
 				listaFinal =l;
 			}		
 		}
@@ -199,16 +186,39 @@ public class Compra {
 	}
 
 	public void calcularCosto() {
-	
+		
 		for(int i = 0; i < listaCompra.size(); i++) {
 			if(listaCompra.get(i).get(2).equals(true)) {
 				costo+= ((Producto) listaCompra.get(i).get(0)).getCosto() * listaCompra.get(i).get(1).hashCode();
 			}
-//			costo+= ((Producto) listaCompra.get(i).get(0)).getCosto() * listaCompra.get(i).get(1).hashCode();
 		}
-		tienda.setPresupuestoCompra(tienda.getPresupuestoCompra() - costo);
+		tienda.setPresupuestoCompra(tienda. getPresupuestoCompra() - costo);
 	}
-
+	
+	public void actualizarBodega() {
+		
+		ArrayList<ArrayList<Object>> l = (ArrayList<ArrayList<Object>>)listaCompra;
+		
+		for(int i = 0; i < l.size(); i++) {
+			int cantidad;
+			if(l.get(i).get(2).equals(true)) {
+				for (int k = 0; k < bodega.getProductosEnBodega().size(); k++) {
+					if (((Producto) l.get(i).get(0)).getCodigo().equals(((Producto)bodega.getProductosEnBodega().get(k).get(0)).getCodigo())) {
+						cantidad =  l.get(i).get(1).hashCode() + bodega.getProductosEnBodega().get(k).get(1).hashCode();
+						bodega.getProductosEnBodega().get(k).set(1, cantidad);
+						break;
+					}
+				}
+			}
+		}
+	}
+	
+	public void setProveedores(ArrayList<Object> proveedores) {this.proveedores = proveedores;}
+	
+	public ArrayList<Object> getProveedores(){return proveedores;}
+	public ArrayList<ArrayList<Object>> getListaCompra(){return listaCompra;}
+	
+	
 	public String toString() {
 		String lista = "";
 		for(int i = 0; i<listaCompra.size(); i++) {
