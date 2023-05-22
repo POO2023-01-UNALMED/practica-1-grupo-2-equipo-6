8afebcafdef51b00f4607ede042526e6d20707c4
Carmen Calderón <cacalderonc@unal.edu.co>
2023-05-22 17:59:35 -0500

    M

--- AdministracionDeTienda/src/uiMain/Interfaz.java
+++ AdministracionDeTienda/src/uiMain/Interfaz.java
@@ -1,12 +1,7 @@
 package uiMain;
 import java.util.ArrayList;
 import java.util.Scanner;
-import gestorAplicacion.clasesBase.Nomina;
-import gestorAplicacion.clasesBase.Producto;
-import gestorAplicacion.clasesBase.Venta;
-import gestorAplicacion.clasesBase.Nomina.meses;
-import gestorAplicacion.clasesHerencia.Empleado;
-import gestorAplicacion.clasesBase.Tienda;
+
 
 public class Interfaz {
 }
