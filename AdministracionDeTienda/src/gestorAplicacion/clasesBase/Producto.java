a71c368a02c15d9bdc6e62d839bf709831f2da0d
Carmen Calderón <cacalderonc@unal.edu.co>
2023-05-24 12:47:11 -0500

    Update Producto.java

--- AdministracionDeTienda/src/gestorAplicacion/clasesBase/Producto.java
+++ AdministracionDeTienda/src/gestorAplicacion/clasesBase/Producto.java
@@ -9,7 +9,7 @@ import gestorAplicacion.clasesBase.Bodega.SETS;
 public class Producto implements Serializable{
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 	private String nombre;
@@ -17,30 +17,30 @@ public class Producto implements Serializable{
 	private double costo;
 	private Tipo tipo;
 
-	
-	
+
+
 	//private double costo;
 
 
-	
-	
+
+
 	public Producto(Tipo tipo) {
 		this.nombre=tipo.toString();
 		this.precio=tipo.getPrecioEstandar();
 		//Constructor para socio
 	}
-	
+
 	public Producto(Tipo tipo, double precio, double costo) {
-		
+
 		this(tipo);
 		this.precio=precio;
 		this.costo = costo;
 		this.tipo=tipo;
-		
-		
+
+
 	}
 
-	
+
 
 	public Producto(Tipo tipo, double costo) {
 		this.tipo=tipo;
@@ -49,7 +49,7 @@ public class Producto implements Serializable{
 	}
 
 	public boolean equals(Producto p) {
-		
+
 		if(p.getNombre()==this.nombre) {
 
 			return true;
@@ -57,20 +57,11 @@ public class Producto implements Serializable{
 		else {
 			return false;
 		}
-		
+
 	}
+
 	
-	public void descontar( float dcto) {
-		if(dcto<0.7 || dcto >=1) {
-			this.setPrecio(getPrecio()*0.9);
-		}
-		else {
-		this.setPrecio(getPrecio()*dcto);
-		}
-	}
-	
-	
-	
+
 	public double getPrecio() {
 		return precio;
 	}
@@ -88,23 +79,23 @@ public class Producto implements Serializable{
 	public String getNombre() {
 		return this.nombre;
 	}
-	
+
 	public void setNombre(String nombre) {
 		this.nombre=nombre;
-		
+
 	}
-	
+
 	public void setCosto(double valor) {costo=valor;}
-	
+
 	public double getCosto() {return costo;}
 
 
 	public enum Tipo{
-		
+
 		CAMISA(25000),PANTALON(30000),ABRIGO(27000);
-		
+
 		private double precioEstandar;
-		
+
 		private Tipo(double precioEstandar) {
 			this.setPrecioEstandar(precioEstandar);
 		}
@@ -116,16 +107,16 @@ public class Producto implements Serializable{
 		public void setPrecioEstandar(double precioEstandar) {
 			this.precioEstandar = precioEstandar;
 		}
-		
-		
-		
+
+
+
 	}
 
 	@Override
 	public String toString() {
 		return "\nTipo: "+nombre+"\nPrecio: "+precio;
 	}
-	
+
 	public Tipo getTipo() {
 		return tipo;
 	}
@@ -133,38 +124,38 @@ public class Producto implements Serializable{
 	public void setTipo(Tipo tipo) {
 		this.tipo = tipo;
 	}
-	
+
 	//Metodos ENVIO
 		public static ArrayList<Producto> seleccionarProductos(SETS set,int cantidad) {
 			ArrayList<Producto> productos=new ArrayList<Producto>();
 			switch(set) {
-			case ONLY: 
+			case ONLY:
 				for(int i=0;i<cantidad;i++) {
 					productos.add(new Producto(Tipo.CAMISA));
-				
+
 			}
 			break;
-			case TU: 
+			case TU:
 				for(int i=0;i<cantidad;i++) {
 					productos.add(new Producto(Tipo.CAMISA));
 					productos.add(new Producto(Tipo.PANTALON));
-				
+
 			}
 			break;
-			case COMPLETO: 
+			case COMPLETO:
 				for(int i=0;i<cantidad;i++) {
 					productos.add(new Producto(Tipo.CAMISA));
 					productos.add(new Producto(Tipo.PANTALON));
 					productos.add(new Producto(Tipo.ABRIGO));
-					
-				
+
+
 			}
 			break;
-			
+
 			}
 			return productos;
 		}
-	    
+
 		public static ArrayList<Producto> clasificar(ArrayList<Producto> productos, Tipo tipo){
 			ArrayList<Producto> clasificados=new ArrayList<Producto>();
 			for(int i=0;i<productos.size();i++) {
@@ -177,7 +168,7 @@ public class Producto implements Serializable{
 		}
 
 
-		
-	
-	
+
+
+
 }
