fbac205ce0a4aa2790a90baae03ee56a73552591
meg63 <mechavarriac@unal.edu.co>
2023-05-24 10:05:31 -0500

    Update Intervenido.java

--- AdministracionDeTienda/src/gestorAplicacion/clasesHerencia/Intervenido.java
+++ AdministracionDeTienda/src/gestorAplicacion/clasesHerencia/Intervenido.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-
+import gestorAplicacion.clasesBase.Producto;
 import AdministracionDeTienda.src.gestorAplicacion.clasesBase.Producto;
 
 
