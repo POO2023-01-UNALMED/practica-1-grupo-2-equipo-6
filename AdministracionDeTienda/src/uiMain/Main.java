f600e25aad88c0d0804b8998247b213809ef5d68
meg63 <mechavarriac@unal.edu.co>
2023-05-24 15:15:54 -0500

    Bajo la responsabilidad de carmen

--- AdministracionDeTienda/src/uiMain/Main.java
+++ AdministracionDeTienda/src/uiMain/Main.java
@@ -23,7 +23,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		valoresIniciales();
+		//valoresIniciales();
 		int opcion = -1;
 		
 		do {
