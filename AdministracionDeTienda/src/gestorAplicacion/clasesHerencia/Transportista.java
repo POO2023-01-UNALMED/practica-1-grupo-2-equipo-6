9a92d8cd2e8c4134ed590827bdefea0e0f016114
meg63 <mechavarriac@unal.edu.co>
2023-05-23 20:21:00 -0500

    Metodo en transportista

--- AdministracionDeTienda/src/gestorAplicacion/clasesHerencia/Transportista.java
+++ AdministracionDeTienda/src/gestorAplicacion/clasesHerencia/Transportista.java
@@ -1,7 +1,12 @@
 package gestorAplicacion.clasesHerencia;
 
 
+import java.util.ArrayList;
+
+import AdministracionDeTienda.src.gestorAplicacion.clasesHerencia.Cliente.Ciudades;
+import AdministracionDeTienda.src.gestorAplicacion.clasesHerencia.Cliente.TipoEnvio;
 import gestorAplicacion.clasesBase.*;
+
 
 public class Transportista extends Persona{
 	private float precioBase, precioDistancia, precioCarga, precioTotal;
@@ -26,5 +31,50 @@ public class Transportista extends Persona{
 	public String toString() {
 		return " nombre :"+ super.getNombre() + "\nprecio del domicilio : " + precioTotal;
 	}
+	
+	//Metodo envio
+	public  Transferencia envioNacional(Cliente cliente, ArrayList<Intervenido> intervenidos, TipoEnvio tipo) {
+			Ciudades ciudad=cliente.getCiudad();
+			double precio;
+			switch(ciudad) {
+			case BARRANQUILLA: precio=2800;
+				break;
+			case BOGOTA: precio=2300;
+				break;
+			case BUCARAMANGA: precio=2900;
+				break;
+			case CALI: precio=2400;
+				break;
+			case CARTAGENA: precio=2700;
+				break;
+			case CUCUTA: precio=2300;
+				break;
+			case MEDELLIN: precio=1500;
+				break;
+			case PEREIRA: precio=2300;
+				break;
+			default: precio=2000;
+				break;
+			
+			}
+			int tamañoPaquete=intervenidos.size();
+			precio+=tamañoPaquete*1000;
+			switch(tipo){
+			case PRIORITARIO: precio+=15000;
+			break;
+			case NORMAL:;
+			break;
+			case LIBRE: precio-=15000;
+			break;	
+			}
+			precio-=precio*cliente.getDescuento();
+			if(precio<0) {
+				precio=0;
+			}
+			Transferencia transferencia=new Transferencia(this.getCuenta(),precio);
+			return transferencia;
+			
+			
+		}
 
 }
