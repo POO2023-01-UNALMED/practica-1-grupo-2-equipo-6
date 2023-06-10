df8353b83652e6a2b8bafea69cc37fa6439c2c08
JohanOrtega28 <125934568+JohanOrtega28@users.noreply.github.com>
2023-06-10 10:38:49 -0500

    Create Transferencia.py

---
+++ Python/src/gestorAplicacion/clasesBase/Transferencia.py
@@ -0,0 +1,70 @@
+class Transferencia:
+    def __init__(self, cuenta, banco, cantidad):
+        self.cantidad = cantidad
+        self.remitente = cuenta
+        self.destinatario = banco
+        self.puntualidadPago = None
+        self.comprobante = False
+        self.detalle = ""
+
+        # LOS PAGOS QUE NO SEAN POR CRÉDITOS, NO VAN EN EL HISTORIAL DE PAGOS
+
+        # Agregarse al historial correspondiente
+        try:
+            banco.historialDePagos[cuenta].append(self)
+        except KeyError:
+            banco.historialDePagos[cuenta] = [self]
+
+        cuenta.transferencias.append(self)
+
+    def isComprobante(self):
+        return self.comprobante
+
+    def setComprobante(self, comprobante):
+        self.comprobante = comprobante
+
+    def getDetalle(self):
+        return self.detalle
+
+    def setDetalle(self, detalle):
+        self.detalle = detalle
+
+    def getPuntualidadPago(self):
+        return self.puntualidadPago
+
+    def setPuntualidadPago(self, estadoPago):
+        self.puntualidadPago = estadoPago
+
+    def realizarTransferencia(self):
+        self.remitente.setDinero(self.remitente.getDinero() - self.cantidad)
+        self.destinatario.setDinero(self.destinatario.getDinero() + self.cantidad)
+        self.comprobante = True
+
+    def __str__(self):
+        s = "Transferencia efectuada.\nCantidad transferida: $" + str(self.cantidad)
+        if isinstance(self.destinatario, Persona):
+            return s + "\nDestinatario: " + self.destinatario.getNombre()
+        elif isinstance(self.destinatario, Socio):
+            return s + "\nDestinatario: " + self.destinatario.getNombre()
+        elif isinstance(self.destinatario, CuentaBancaria):
+            return s + "\nDestinatario: Cuenta " + self.destinatario.getIBAN()
+
+        return s
+
+    def getRemitente(self):
+        return self.remitente
+
+    def setRemitente(self, remitente):
+        self.remitente = remitente
+
+    def getDestinatario(self):
+        return self.destinatario
+
+    def setDestinatario(self, destinatario):
+        self.destinatario = destinatario
+
+
+class EstadoPago:
+    A_TIEMPO = "A_TIEMPO"
+    RETRASADO = "RETRASADO"
+
