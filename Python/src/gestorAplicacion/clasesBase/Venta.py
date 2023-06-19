3c12efe1125a2dd7d9dd368f4b5713238581ee38
Carmen Calderón <cacalderonc@unal.edu.co>
2023-06-19 15:51:28 -0500

    Update Venta.py

--- Python/src/gestorAplicacion/clasesBase/Venta.py
+++ Python/src/gestorAplicacion/clasesBase/Venta.py
@@ -2,9 +2,9 @@ import pickle
 
 
 class Venta:
-    porcentaje_banco = 0
+    porcentajeBanco = 0
 
-    def _init_(self, comprador):
+    def __init__(self, comprador):
         self.productos_venta = []
         self.total = 0
         self.comprador = comprador
@@ -12,62 +12,62 @@ class Venta:
         self.ganancias = 0
         self.repartidor = None
 
-    @staticmethod
-    def asignar_porcentaje_banco(venta):
-        if Venta.porcentaje_banco != 0:
-            CuentaTienda.get_cuenta_tienda().get_entidad().abonar_cuenta_auxiliar(
-                Venta.porcentaje_banco * venta.ganancias
+    @classmethod
+    def asignarPorcentajeBanco(cls,venta):
+        if cls.porcentajeBanco != 0:
+            Tienda.getCuentaTienda().getEntidad().abonarCuentaAuxiliar(
+                cls.porcentajeBanco * venta.getGanancias()
             )
-            CuentaTienda.get_cuenta_tienda().set_dinero(
-                CuentaTienda.get_cuenta_tienda().get_dinero() - Venta.porcentaje_banco * venta.ganancias
+            Tienda.getCuentaTienda().setDinero(
+                Tienda.getCuentaTienda().getDinero() - cls.porcentajeBanco * venta.getGanancias()
             )
 
-    def get_total(self):
+    def getTotal(self):
         return self.total
 
-    def set_total(self, total):
+    def setTotal(self, total):
         self.total = total
 
-    def get_productos_venta(self):
+    def getProductosVenta(self):
         return self.productos_venta
 
-    def set_productos_venta(self, productos_venta):
+    def setProductosVenta(self, productos_venta):
         self.productos_venta = productos_venta
 
-    def get_comprador(self):
+    def getComprador(self):
         return self.comprador
 
-    def set_comprador(self, comprador):
+    def setComprador(self, comprador):
         self.comprador = comprador
 
-    def get_productos_ofertados(self):
+    def getProductosOfertados(self):
         return self.productos_ofertados
 
-    def set_productos_ofertados(self, productos_ofertados):
+    def setProductosOfertados(self, productos_ofertados):
         self.productos_ofertados = productos_ofertados
 
-    def get_ganancias(self):
+    def getGanancias(self):
         return self.ganancias
 
-    def set_ganancias(self, ganancias):
+    def setGanancias(self, ganancias):
         CuentaTienda.get_cuenta_tienda().set_dinero(ganancias + CuentaTienda.get_cuenta_tienda().get_dinero())
         self.ganancias = ganancias
 
-    def get_repartidor(self):
+    def getRepartidor(self):
         return self.repartidor
 
-    def set_repartidor(self, repartidor):
+    def setRepartidor(self, repartidor):
         self.repartidor = repartidor
 
-    @staticmethod
-    def get_porcentaje_banco():
-        return Venta.porcentaje_banco
+    @classmethod
+    def getPorcentajeBanco(cls):
+        return cls.porcentajeBanco
 
-    @staticmethod
-    def set_porcentaje_banco(porcentaje_banco):
-        Venta.porcentaje_banco = porcentaje_banco
+    @classmethod
+    def setPorcentajeBanco(cls, porcentajeBanco):
+        cls.porcentajeBanco = porcentajeBanco
 
-    def _str_(self):
+    def __str__(self):
         s = ""
         try:
             for producto in self.productos_venta:
