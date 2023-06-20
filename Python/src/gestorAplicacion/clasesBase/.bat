d46638233eeb85d1bfd1e49061ce59ad957383e6
meg63 <mechavarriac@unal.edu.co>
2023-06-20 11:34:24 -0500

    a

---
+++ Python/src/gestorAplicacion/clasesBase/.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\DELL\AppData\Local\Programs\Python\Python311\python.exe" C:\Users\DELL\Documents\GitHub\practica-1-grupo-2-equipo-6\Python\src\gestorAplicacion\clasesBase\main.py
+pause
